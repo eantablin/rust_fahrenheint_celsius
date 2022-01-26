003de28a577498d6f59d40a9dbbc37762b77ee9c
eantablin <eantablin@protonmail.com>
2022-01-26 10:12:06 -0800

    Adds c2f

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
 use std::io;
 
 fn main() {
-    println!("Let's convert to celsius");
-    println!("Enter temp in fahrenheit: ");
+    dialogue();
+}
 
-    let u_input = f2c();
-    println!("That's {} in celsius", u_input);
+fn dialogue() {
+
+    let mut u_input = String::new();
+
+    println!("What's our return value? Fahrenheit or celsius?");
+    
+    io::stdin()
+        .read_line(&mut u_input)
+        .expect("Failed to read input");
+    
+    for i in u_input.chars() { // Yes, the hard way.
+        if i == 'f'|| i == 'F' {
+            println!("That's {} in celsius", f2c());
+            break;
+        } else if i == 'c' || i == 'C' {
+            println!("That's {} in fahrenheit", c2f());
+            break;
+        }
+    }
+
 }
 
 fn f2c() -> f64 {
+
+    println!("Please input fahrenheit.");
     let mut temp = String::new();
 
     io::stdin()
@@ -23,8 +43,32 @@ fn f2c() -> f64 {
             f2c()
         }
     };
-
+    
     temp = (temp - 32_f64) * (5_f64/9_f64); // Do some quick math, have to specify floats
 
     temp // Return manipulated variable
+}
+
+fn c2f() -> f64 {
+
+    println!("Please input celsius.");
+
+    let mut temp = String::new();
+
+    io::stdin()
+        .read_line(&mut temp)
+        .expect("Failed to read input");
+
+    let mut temp: f64 = match temp.trim().parse() {
+
+        Ok(num) => num,
+        Err(e) => {
+            println!("Please input a number {}", e);
+            c2f()
+        }
+    };
+
+    temp = (temp * 9_f64/5_f64) + 32_f64;
+
+    temp
 }
